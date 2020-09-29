dd198a67fd4fa017b82c2c667c99c9bd505fb6e2
matthew-devonport <roarsintheeast@gmail.com>
2020-09-30 11:59:54 +1300

    declared usestate hooks and called in useeffect function

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -3,9 +3,14 @@ import queryString from 'query-string'
 import io from 'socket.io-client'
 
 const Chat = ({ location }) => {
-   
+    const [name, setName] = useState('');
+    const [room, setRoom] = useState('');
+
     useEffect(() => {
-        const { name, room } = queryString.parse(location.search)
+        const { name, room } = queryString.parse(location.search);
+
+        setName(name);
+        setRoom(room);
     })
 
     return (
