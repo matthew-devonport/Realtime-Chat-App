01f514e65ad8e5e8c1a16af973f7198e8c8f9ca9
matthew-devonport <roarsintheeast@gmail.com>
2020-09-30 11:58:13 +1300

    setup useEffect function

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -2,7 +2,12 @@ import React, { useState, useEffect } from 'react'
 import queryString from 'query-string'
 import io from 'socket.io-client'
 
-const Chat = () => {
+const Chat = ({ location }) => {
+   
+    useEffect(() => {
+        const { name, room } = queryString.parse(location.search)
+    })
+
     return (
         <h1>Chat</h1>
     )
