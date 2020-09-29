4046c8e9525355c5889930ba44c9858672187f1a
matthew-devonport <roarsintheeast@gmail.com>
2020-09-29 17:43:27 +1300

    setup html in join component

--- client/src/components/Join/Join.js
+++ client/src/components/Join/Join.js
@@ -7,9 +7,18 @@ const Join = () => {
   const [name, setName] = useState('');
   const [room, setRoom] = useState('');
 
-    return (
-        <h1>Join</h1>
-    )
+  return (
+    <div className="joinOuterContainer">
+      <div className="joinInnerContainer">
+        <h1 className="heading">Join</h1>
+        <div><input placeholder="Name" className="joinInput" type="text" onChange={} /></div>
+        <div><input placeholder="Room" className="joinInput mt-20" type="text" onChange={} /></div>
+        <Link>
+          <button className="button mt-20" type="submit">Sign In</button>
+        </Link>
+      </div>
+    </div>
+  )
 }
 
 export default Join
