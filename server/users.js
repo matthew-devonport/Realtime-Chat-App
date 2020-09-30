1ce0e802f52e9393c27f8a216196fa3e6826d8ab
matthew-devonport <roarsintheeast@gmail.com>
2020-09-30 14:17:26 +1300

    exporting user.js functions

--- server/users.js
+++ server/users.js
@@ -27,7 +27,6 @@ const removeUser = (id) => {
 
 const getUser = (id) => users.find((user) => user.id === id);
 
-
-
 const getUsersInRoom = (room) => users.filter((user) => user.room === room);
 
+module.exports = { addUser, removeUser, getUser, getUsersInRoom };
