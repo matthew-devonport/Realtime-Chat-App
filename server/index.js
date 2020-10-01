260ee1d3597970233eb51d57f895c64a6698c1a9
matthew-devonport <roarsintheeast@gmail.com>
2020-10-01 23:06:31 +1300

    added to function so user can send message/callback to front end

--- server/index.js
+++ server/index.js
@@ -28,6 +28,10 @@ io.on('connection', (socket) => {
 
     socket.on('sendMessage', (message, callback) => {
         const user = getUser(socket.id)
+
+        io.to(user.room).emit('message', { user: user.name, text: message})
+
+        callback();
     })
 
     socket.on('disconnect', () => {
