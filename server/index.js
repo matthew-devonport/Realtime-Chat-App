426e5516df05c92c8690c08e95c8ffccc6a411c2
matthew-devonport <roarsintheeast@gmail.com>
2020-10-01 22:58:58 +1300

    added callback error

--- server/index.js
+++ server/index.js
@@ -16,6 +16,8 @@ const io = socketio(server)
 io.on('connection', (socket) => {
  socket.on('join', ({ name, room }, callback) => {
      const { error, user } = addUser({ id: socket.id, name, room });
+
+     if(error) return callback(error)
  })
 
  socket.on('disconnect', () => {
