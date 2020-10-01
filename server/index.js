2ac2064ff99e95a2ed3d1119316eac40fc6fca71
matthew-devonport <roarsintheeast@gmail.com>
2020-10-01 23:03:06 +1300

    added message to current users new user has joined

--- server/index.js
+++ server/index.js
@@ -19,8 +19,8 @@ io.on('connection', (socket) => {
 
      if(error) return callback(error);
 
-     socket.emit('message', { user: 'admin', text: `${user.name}, welcome to the room ${user.room }` })
-
+     socket.emit('message', { user: 'admin', text: `${user.name}, welcome to the room ${user.room }` });
+     socket.broadcast.to(user.room).emit('message', { user: 'admin', text: `${user.name}, has joined`});
      socket.join(user.room);
  })
 
