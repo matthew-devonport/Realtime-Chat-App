0b667288e97f060d2df27c3b07352585d56aafa9
matthew-devonport <roarsintheeast@gmail.com>
2020-10-01 22:58:31 +1300

    added arguments to addUser function

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ const io = socketio(server)
 
 io.on('connection', (socket) => {
  socket.on('join', ({ name, room }, callback) => {
-     const { error, user } = addUser();
+     const { error, user } = addUser({ id: socket.id, name, room });
  })
 
  socket.on('disconnect', () => {
