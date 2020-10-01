2b070fe96c46571eaefc064b112dc4a01cf7ce9e
matthew-devonport <roarsintheeast@gmail.com>
2020-10-01 22:57:34 +1300

    added addUser function on join

--- server/index.js
+++ server/index.js
@@ -14,10 +14,8 @@ const io = socketio(server)
 
 
 io.on('connection', (socket) => {
- console.log('We have a new connection!!!');
-
  socket.on('join', ({ name, room }, callback) => {
-     console.log(name, room);
+     const { error, user } = addUser();
  })
 
  socket.on('disconnect', () => {
