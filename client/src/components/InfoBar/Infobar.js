eacc11e4504c35dfa8097deec48218f1b94a49b5
matthew-devonport <roarsintheeast@gmail.com>
2020-10-04 12:55:43 +1300

    added/imported icons

--- client/src/components/InfoBar/Infobar.js
+++ client/src/components/InfoBar/Infobar.js
@@ -1,18 +1,21 @@
 import React from 'react';
 
+import closeIcon from '../../icons/closeIcon'
+import onlineIcon from '../../icons/onlineIcon'
+
 import './InfoBar.css'
 
-const Infobar = () => {
+const InfoBar = () => {
     <div className="infoBar">
         <div className="leftInnerContainer">
-
+            <img className="onlineIcon" src={onlineIcon} alt="online image" />
+            <h3>roomName</h3>
         </div>
         <div className="rightInnerContainer">
-
+            <a href="/"><img src={closeIcon} alt="close image" /></a>
         </div>
 
     </div>
-
 }
 
-export default Infobar;
+export default InfoBar;
