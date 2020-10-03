64c7660600c10d8c83c17856e236ae19f2ce62b2
matthew-devonport <roarsintheeast@gmail.com>
2020-10-04 12:28:12 +1300

    added chat css styles/imported styles to chat.js

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from 'react'
 import queryString from 'query-string'
 import io from 'socket.io-client'
 
+import './Chat.css';
+
 let socket;
 
 const Chat = ({ location }) => {
