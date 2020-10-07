a45d33ef1a1ab6e352792a267e2155ed7d1defd0
matthew-devonport <roarsintheeast@gmail.com>
2020-10-08 12:08:27 +1300

    setup if statement and true showing

--- client/src/components/Messages/Message/Message.js
+++ client/src/components/Messages/Message/Message.js
@@ -2,10 +2,31 @@ import React from 'react';
 
 import './Message.css'
 
-const Message = ({ messages, name }) => (
-<div>
+const Message = ({ message: {user, text}, name }) => {
+    let isSentByCurrentUser = false;
 
-</div>
-)
+    const trimmedName = name.trim().toLowerCase();
+
+    if (user === trimmedName) {
+        isSentByCurrentUser = true;
+    }
+
+    return (
+        isSentByCurrentUser
+            ? (
+                <div className="messageContainer">
+                    <p className="sentText">{trimmedName}</p>
+                    <div className="messageBox"></div>
+                    <p className="messageText">{text}</p>
+                </div>
+            )
+            : (
+       <div>
+           {/*  */}
+       </div>
+      )
+    )
+
+}
 
 export default Message;
